642e90301bbbd61e953876d32c8a833479522664
André Luiz Brandt <andre-luiz.b@unifei.edu.br>
2021-03-13 19:26:08 -0300

    Vuex, axios and router install

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import axios from 'axios';
+import router from './router'
+import Vuex from 'vuex'
+//vue-mask
 
-Vue.config.productionTip = false
+
+Vue.use(Vuex)
+Vue.use(axios);
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
